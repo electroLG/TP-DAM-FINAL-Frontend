1ff9644a119feea21acb4633de6b2c336b620b2f
ipciot <electroroelg@gmail.com>
2022-10-31 14:21:21 -0300

    Agredado gráfico de 2 vars + botones de seleccion + regen grafico

--- src/app/grafico/grafico.page.ts
+++ src/app/grafico/grafico.page.ts
@@ -5,6 +5,7 @@ import * as Highcharts from 'highcharts';
 require('highcharts/highcharts-more')(Highcharts);
 require('highcharts/modules/data')(Highcharts);
 require('highcharts/modules/exporting')(Highcharts);
+require('highcharts/modules/boost')(Highcharts);
 require('highcharts/modules/export-data')(Highcharts);
 require('highcharts/modules/accessibility')(Highcharts);
 
@@ -16,9 +17,13 @@ require('highcharts/modules/accessibility')(Highcharts);
 export class GraficoPage implements OnInit {
   logs: any;
   datagraf: Array<Array<number>> = new Array<Array<number>>();
+  datagraf2: Array<Array<number>> = new Array<Array<number>>();
+  btnGraficoDis: boolean;
   public mygraph;
   public graphOptions;
   constructor(public conndb: ApiConnService,private activatedRoute: ActivatedRoute) {
+
+  this.btnGraficoDis=false; //Arranco mostrando los botones
 
   }
 
@@ -27,36 +32,42 @@ export class GraficoPage implements OnInit {
 
   ionViewDidEnter() {
      this.generarGraficoTepelco();
-    //  this.updateChartTepelco();
-    // setTimeout(()=>{
-    //    this.updateChartTepelco();
-    //  },1000);
+
   }
   ionViewWillEnter() {
   }
 
   async mostrarTepelcoLogs()
   {
+      this.regenerarGrafico();
+      //console.log('Pido datos historicos');
       this.logs=await this.conndb.getTepelcoLogs();
-      console.log(this.logs);
+      //console.log('Llegaron datos historicos');
+      //console.log(this.logs);
       this.convertirDatos();
   }
   async mostrarTepelcoLogsSemana()
   {
+      this.regenerarGrafico();
+      //console.log('Pido datos de la semana');
       this.logs=await this.conndb.getTepelcoLogsSemana();
-      console.log(this.logs);
+      //console.log('Llegaron datos de la semana');
+      //console.log(this.logs);
       this.convertirDatos();
   }
     async mostrarTepelcoLogsDia()
   {
+      this.regenerarGrafico();
+      //console.log('Pido datos del día');
       this.logs=await this.conndb.getTepelcoLogsDia();
-      console.log(this.logs);
+      //console.log('Llegaron datos del día');
+      //console.log(this.logs);
       this.convertirDatos();
   }
 
   clearTepelcoLogs()
   {
-      this.datagraf=[[0,0]];
+      this.regenerarGrafico();
       this.updateChartTepelco();
   }
 
@@ -72,19 +83,47 @@ convertirDatos(){
                                 Number(this.logs[i].fecha.substring(14,16)),
                                 Number(this.logs[i].fecha.substring(17,19))),
                                 Number(this.logs[i].dp_cartucho)];
+
+    // this.datagraf2[i]=[Date.UTC(Number(this.logs[i].fecha.substring(0,4)),
+    //                             Number(this.logs[i].fecha.substring(5,7))-1,
+    //                             Number(this.logs[i].fecha.substring(8,10)),
+    //                             Number(this.logs[i].fecha.substring(11,13)),
+    //                             Number(this.logs[i].fecha.substring(14,16)),
+    //                             Number(this.logs[i].fecha.substring(17,19))),
+    //                             Number(this.logs[i].dp_filtro)];
+
+      this.datagraf2[i]=[this.datagraf[i][0],Number(this.logs[i].dp_filtro)];
+
    }
    console.log(this.datagraf);
    this.updateChartTepelco();
 }
 
 updateChartTepelco(){
+
   this.mygraph.update({
-    series: [{
-      type: 'area',
-      name: 'kPA',
-      data: this.datagraf
-    }]
+   series: [{
+      //type: 'area',
+      data: this.datagraf,
+      name: "Cartucho"
+    },
+    {
+      //type: 'area',
+      data: this.datagraf2,
+      name: "Filtro"
+    }
+  ]
 });
+this.btnGraficoDis=false;
+}
+
+regenerarGrafico(){
+  this.btnGraficoDis=true;
+  this.mygraph.destroy();
+  this.generarGraficoTepelco();
+  this.datagraf=[[0,0]];
+  this.datagraf2=[[0,0]];
+  this.logs=[[0,0]];
 }
 
 generarGraficoTepelco(){
@@ -101,12 +140,15 @@ generarGraficoTepelco(){
     },
     xAxis: {
       type: 'datetime',
-      crosshair: true
+      crosshair: true,
+      title: {
+        text: 'Timestamp'
+      }
     },
     yAxis: {
       crosshair: true,
       title: {
-        text: 'Diferencial de presión'
+        text: 'Diferencial de presión [kPa]'
       }
     },
     legend: {
@@ -140,10 +182,16 @@ generarGraficoTepelco(){
     },
 
     series: [{
-      type: 'area',
-      name: 'kPA',
-      data: this.datagraf
-    }]
+      //type: 'area',
+      data: this.datagraf,
+      name: "Cartucho"
+    },
+    {
+      //type: 'area',
+      data: this.datagraf2,
+      name: "Filtro"
+    }
+  ]
   };
   this.mygraph = Highcharts.chart('container3', this.graphOptions );
 }
